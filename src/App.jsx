55d8a5be8a2fad8870a548ab8a249645eff5fdc1
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-05-02 11:27:33 +0545

    :lipstick: form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 class App extends Component {
-  state = { }
+  constructor(){
+    super()
+    this.state = {
+      fullname: '',
+      username: '',
+      email: '',
+      password: ''
+    }
+  }
   render() {
-    return( );
+    return( 
+      <div>
+        <div className="container">
+          <div className="form-div">
+            <form>
+              <input type= 'text' placeholder="Full Name" onchange = {this.changeFullName} 
+              value={this.state.fullname} className='form=control form-group'/>
+
+              <input type= 'text' placeholder="User Name" onchange = {this.changeUserName} 
+              value={this.state.username} className='form=control form-group'/>
+
+              <input type= 'text' placeholder="Email" onchange = {this.changeEmail} 
+              value={this.state.email} className='form=control form-group'/>
+
+              <input type= 'text' placeholder="Password" onchange = {this.changePassword} 
+              value={this.state.password} className='form=control form-group'/>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
